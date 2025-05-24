ad1b8845b82ca3d0530a9e3422c58547cb77580d
Merlin Rosales <merlinrosales96@gmail.com>
2025-05-23 23:58:34 -0500

    Update App.

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,30 @@ import Navbar from './components/layout/Navbar2';
 import Home from './pages/Home';
 import Footer from './components/layout/Footer';
 import './App.css';
+import { Box } from '@mui/material';
+import NotFound from './pages/NotFound';
 
 function App() {
 
   return (
     <ThemeProviderWrapper>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-      <Footer />
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
+      >
+        <Navbar />
+        <Box sx={{ flex: 1 }}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Box>
+        <Footer />
+      </Box>
     </ThemeProviderWrapper>
   )
 }
